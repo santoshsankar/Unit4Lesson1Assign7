f4c5fcce906898920e5eb807e440e54c579a4282
santoshsankar <santoshsankar@gmail.com>
2014-10-14 22:17:24 -0400

    Web Svc Ex

--- Web Service Example/ViewController.swift
+++ Web Service Example/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myNameLabel: UILabel!
+    @IBOutlet weak var loadSpinner: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let manager = AFHTTPRequestOperationManager()
+
+        manager.GET( "http:/graph.facebook.com/ssankar2010",
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                println("JSON: " + responseObject.description)
+            
+                if let myName = responseObject.valueForKey("name") as? String {
+                    self.loadSpinner.startAnimating();
+                    self.loadSpinner.stopAnimating();
+                    self.loadSpinner.hidesWhenStopped = 1;
+                    self.myNameLabel.text = myName;
+                }
+            
+            },
+            failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println("Error: " + error.localizedDescription)
+        })
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ bridge.h
@@ -0,0 +1,15 @@
+//
+//  bridge.h
+//  Web Service Example
+//
+//  Created by Santosh Sankar on 10/14/14.
+//  Copyright (c) 2014 Santosh Sankar. All rights reserved.
+//
+
+#ifndef Web_Service_Example_bridge_h
+#define Web_Service_Example_bridge_h
+
+#import "AFNetworking/AFNetworking.h"
+
+
+#endif
